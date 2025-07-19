02cba701e928ba1a1b15b7fe4ccc5f01b36a65ec
Kirito_Tempest <tanawattakopalm@gmail.com>
2025-07-19 18:24:29 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   api2/app.py

--- api2/app.py
+++ api2/app.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO)
 @app.route("/api")
 def hello():
     logging.info("[API2] Received request from API1")
-    return jsonify({"source": "API2", "message": "Hello World from API2"})
+    return jsonify({"source": "API2", "message": "Hello Hackathon from API2"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
